8629083bcd1a982774a79f90d3c433e1d0679442
guanqun <guanqun.lu@gmail.com>
2013-04-17 22:45:23 +0800

    add anchorpoint property

--- convert2ccx.py
+++ convert2ccx.py
@@ -35,6 +35,7 @@ properties = [ 'position',
                'contentSize',
                'tag',
                'rotation',
+               'anchorPoint',
                'zOrder'
              ]
 
